f3211b01d4fbc05caa0387d2d827385cbcbc1a93
Arun Lohar <arunlohar@gkmit.co>
2024-08-11 19:17:05 +0530

    all added

---
+++ s3-static-host.tf
@@ -0,0 +1,50 @@
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = "jenkins-s3-bucket-for-index-html-testing-9414421231"
+}
+
+resource "aws_s3_bucket_public_access_block" "publicallow" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "website_bucket_policy" {
+  bucket = aws_s3_bucket.website_bucket.id
+  depends_on = [aws_s3_bucket_public_access_block.publicallow]
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid = "PublicReadGetObject"
+        Effect = "Allow"
+        Principal = "*"
+        Action = "s3:GetObject"
+        Resource = "${aws_s3_bucket.website_bucket.arn}/*"
+      },
+    ]
+  })
+}
+
+resource "aws_s3_object" "index" {
+  bucket        = aws_s3_bucket.website_bucket.bucket
+  key           = "index.html"
+  source        = "./index.html"
+  content_type  = "text/html"
+}
+
+
+resource "aws_s3_bucket_website_configuration" "web-host-config" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+output "s3_url" {
+  description = "s3 obj url"
+  value       = aws_s3_bucket_website_configuration.web-host-config.website_endpoint
+}
---
+++ terraform-awscli-git.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+echo "apt update"
+sudo apt-get update
+
+echo "install curl wget unzip"
+sudo apt-get install -y curl wget unzip
+
+echo "install git"
+sudo apt-get install -y git
+
+echo "install awscli"
+sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo unzip awscliv2.zip
+sudo ./aws/install --update
+aws --version
+rm awscliv2.zip
+
+echo "Installing terraform"
+sudo apt-get install -y gnupg software-properties-common
+
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+
+gpg --no-default-keyring \
+--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--fingerprint
+
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+sudo apt-get update
+sudo apt-get install -y terraform
