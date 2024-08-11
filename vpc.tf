b4b97d23df7e191fa21b589b97a3b0bb9aedcbf1
Arun Lohar <arunlohar@gkmit.co>
2024-08-11 21:11:05 +0530

    all

---
+++ vpc.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "jenkins-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-south-1a"]
+  public_subnets  = ["10.0.101.0/24"]
+
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+  }
+}
+
